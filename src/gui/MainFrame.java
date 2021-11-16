1f0aa3257c7612f305d93ff4aabbd9e14fdfbfa6
Nikola Kalinić <kalinic.ra71.2019@uns.ac.rs>
2021-11-16 15:39:38 +0100

    Added mainframe feature

---
+++ src/gui/MainFrame.java
@@ -0,0 +1,27 @@
+package gui;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class MainFrame extends JFrame{
+	
+	public MainFrame(){
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Dimension d = kit.getScreenSize();
+		int sHeight = d.height;
+		int sWidth = d.width;
+		setSize(3*sWidth/4,3*sHeight/4);
+		
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setTitle("Studenska služba");
+		
+		setVisible(true);
+		
+		
+	}
+
+}
--- src/main/MyApp.java
+++ src/main/MyApp.java
@@ -1,10 +1,11 @@
 package main;
 
+import gui.MainFrame;
+
 public class MyApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MainFrame mf = new MainFrame();
 	}
 
 }
